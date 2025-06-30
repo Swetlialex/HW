fb3619a49ba9c198aa599d908167b86d78701140
Swetlialex <swetlialex@gmail.com>
2025-06-30 10:26:49 +0300

    Add files via upload
    Поправено име на променлива.

--- acad_to_astro_hours.py
+++ acad_to_astro_hours.py
@@ -1,17 +1,17 @@
 academic_hour_minutes = 40
 break_minutes = 20
-session_legnth = 4 # 4 академични часа за сесия
+session_length = 4 # 4 академични часа за сесия
 astronomical_hour_minutes = 60
 academic_hours_total = 64 # Общо академични часове на курса
 
 # Продължителност на една сесия в минути
-sessions = session_legnth*academic_hour_minutes + break_minutes
+sessions = session_length*academic_hour_minutes + break_minutes
 
 # Продължителност на една сесия в астрономически часове
 sessions_astronomical = sessions / astronomical_hour_minutes
 
 # Брой сесии
-sessions_total = academic_hours_total / session_legnth
+sessions_total = academic_hours_total / session_length
 
 #Брой астрономически часове
 astronomical_hours_total = sessions_total * sessions_astronomical
