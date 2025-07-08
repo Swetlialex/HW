a50511e089386fc4bad2b73833b62bcaed2e7c8d
Swetlialex <swetlialex@gmail.com>
2025-07-08 17:07:35 +0300

    Домашна работа MIN MAX
    I решение със списъци

---
+++ task_min_max_student_score.py
@@ -0,0 +1,31 @@
+student_score = {
+                'Ivan'  :5.00,
+                'Alex'  :3.50,
+                'Maria' :5.50,
+                'Georgy':5.00
+                }
+# преобразувам ключовете в списък и стойностите в списък
+for key, value in student_score.items():
+    lst_key = list(student_score.keys())
+    lst_value = list(student_score.values())
+
+# print(lst_key)
+# print(lst_value)
+
+# намирам максималната и минималната стойности в списъка със стойности
+max_value = max(lst_value)
+# print(max_value)
+min_value = min(lst_value)
+# print(min_value)
+
+# отпечатвам max              
+for key, value in student_score.items():
+    if value == max_value:
+        print(f'{key} - {value}')
+        
+# отпечатвам max и min  
+for key, value in student_score.items():
+    if value ==min_value:
+        print(f'{key} - {value}')
+
+
