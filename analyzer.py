d7284a3344eb57f56a8fed43015d43b59cfa3f4f
Swetlialex <swetlialex@gmail.com>
2025-08-04 18:10:17 +0300

    Delete analyzer.py
    -

--- analyzer.py
+++
@@ -1,7 +0,0 @@
-# data_processing/analyzer.py
-
-def calculate_average(data):
-    """Изчислява средната стойност на даден списък."""
-    if not data:
-        return 0
-    return sum(data) / len(data)
